0ea0bd591c04f349c1548f398cbf643c17060adf
pepe-roni <ahde2k@gmail.com>
2018-07-22 00:12:20 -0700

    algorithm for counting time

--- binarcyclk/binarcyclk.ino
+++ binarcyclk/binarcyclk.ino
@@ -7,16 +7,39 @@ int minPins[] = {8,9,10,11,12,13};
 int hourPins[] = {14,15,16,17,18,19};
 
 //Set the Upload Time, the local time at which the arduino will start ticking at
-int startSec = 0;
-int startMin = 35;
-int startHour = 18;
+int sec = 0;
+int mins = 35;
+int hour = 18;
 
 void setup() {
-  // put your setup code here, to run once:
-
+  //sets up all LED pins to output
+  for(int i=0; i<6;i++)
+  {
+    pinMode(secPins[i], OUTPUT);
+    pinMode(minPins[i], OUTPUT);
+    pinMode(hourPins[i], OUTPUT);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+//Runs repeatedly, adjust the delay to compensate for +- in clock speeds
+void loop(){
+  sec++;
+  //everytime sec is 60, mins increases by one and sec is reset
+  if(sec == 60){
+    sec = 0 ;
+    mins++;
+    //everytime mins is 60, hour increases by one and mins is reset
+    if(mins == 60){
+      mins = 0;
+      hour++;
+      if(hour == 24){
+        //when hour is 24, all time variables are reset
+        hour = 0;
+        sec = 0;
+        mins = 0;
+      }
+    }
+  }
+ 
 
 }
