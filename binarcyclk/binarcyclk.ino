60b15126f55609fe86e42f0ccb5e8328b70f03f6
pepe-roni <ahde2k@gmail.com>
2018-07-22 00:59:47 -0700

    removed serial (at least for now) added algorithms for displaying binary changing states VIA bitwise AND operators

--- binarcyclk/binarcyclk.ino
+++ binarcyclk/binarcyclk.ino
@@ -1,15 +1,15 @@
 //array of LED pins for seconds and minutes(PWM)
 //skip pins 1 and 0, reserved for TX and RX
-unsigned char secPins[] = {2, 3, 4, 5, 6, 7};
-unsigned char minPins[] = {8, 9, 10, 11, 12, 13};
+uint8_t secPins[] = {2, 3, 4, 5, 6, 7};
+uint8_t minPins[] = {8, 9, 10, 11, 12, 13};
 
 //the LED pins for hours are on analog pins 0-5 (PWM)
-unsigned char hourPins[] = {14, 15, 16, 17, 18, 19};
+uint8_t hourPins[] = {14, 15, 16, 17, 18, 19};
 
 //Set the Upload Time, the local time at which the arduino will start ticking at
-unsigned char sec = 0;
-unsigned char mins = 35;
-unsigned char hour = 18;
+uint8_t sec = 0;
+uint8_t mins = 35;
+uint8_t hour = 18;
 
 void setup() {
   //sets up all LED pins to output
@@ -19,7 +19,6 @@ void setup() {
     pinMode(minPins[i], OUTPUT);
     pinMode(hourPins[i], OUTPUT);
   }
-  Serial.begin(9400);
 }
 
 //Runs repeatedly, adjust the delay to compensate for +- in clock speeds
@@ -41,14 +40,31 @@ void loop() {
       }
     }
   }
-  Serial.print(hour);
-  Serial.print(":");
-  Serial.print(mins);
-  Serial.print(":");
-  Serial.print(sec);
-  Serial.println();
-  
+
+  //change this if your clock runs slower or faster than expected
   delay(1000);
+}
 
+//displays the second LEDs 
+void secDisp(unsigned char secBinary){
+  //this switches the bits according to the time input using a bitwise AND operator (&)
+  for(unsigned char i = 0; i < sizeof(secPins); i++){
+    digitalWrite(secPins[i], secBinary &1);
+    secBinary /=2;
+  }
+}
 
+void minsDisp(unsigned char minsBinary){
+  //this switches the bits according to the time input using a bitwise AND operator (&)
+  for(unsigned char i = 0; i < sizeof(minPins); i++){
+    digitalWrite(minPins[i], minsBinary &1);
+    minsBinary /=2;
+  }
+}
+void hourDisp(unsigned char hourBinary){
+  //this switches the bits according to the time input using a bitwise AND operator (&)
+  for(unsigned char i = 0; i < sizeof(hourPins); i++){
+    digitalWrite(hourPins[i], hourBinary &1);
+    hourBinary /=2;
+  }
 }
