91d6f7acf25d42b103328146aa0f8603fd12fd32
pepe-roni <ahde2k@gmail.com>
2018-07-22 00:37:49 -0700

    switched to unsigned chars for efficiency

--- binarcyclk/binarcyclk.ino
+++ binarcyclk/binarcyclk.ino
@@ -1,19 +1,19 @@
 //array of LED pins for seconds and minutes(PWM)
 //skip pins 1 and 0, reserved for TX and RX
-int secPins[] = {2, 3, 4, 5, 6, 7};
-int minPins[] = {8, 9, 10, 11, 12, 13};
+unsigned char secPins[] = {2, 3, 4, 5, 6, 7};
+unsigned char minPins[] = {8, 9, 10, 11, 12, 13};
 
 //the LED pins for hours are on analog pins 0-5 (PWM)
-int hourPins[] = {14, 15, 16, 17, 18, 19};
+unsigned char hourPins[] = {14, 15, 16, 17, 18, 19};
 
 //Set the Upload Time, the local time at which the arduino will start ticking at
-int sec = 0;
-int mins = 35;
-int hour = 18;
+unsigned char sec = 0;
+unsigned char mins = 35;
+unsigned char hour = 18;
 
 void setup() {
   //sets up all LED pins to output
-  for (int i = 0; i < 6; i++)
+  for (unsigned char i = 0; i < 6; i++)
   {
     pinMode(secPins[i], OUTPUT);
     pinMode(minPins[i], OUTPUT);
